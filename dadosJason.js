d598fb5217584feaeb8adea637c402c912dfd3fb
Adilson Dias <adilsondias@outlook.com>
2020-10-21 11:24:18 -0300

    Corrigo bug

--- dadosJason.js
+++ dadosJason.js
@@ -1,20 +1,12 @@
 const fs = require('fs')
-const fetch = require('node-fetch')
-// fetch('https://github.com/')
-//     .then(res => res.text())
-//     .then(body => console.log(body));
+const fetch = require('node-fetch') 
 
-
-// console.log(writerData());    
 fetch('https://api.chucknorris.io/jokes/random')
     .then(res => res.json())
     .then( data => writerData(data))
-
-
-    const writerData = (data)=>{        
+    const writerData = (data)=>{       
 
         fs.appendFileSync('dados.json',  JSON.stringify(data.value))
-
-        console.log(JSON.stringify(data));
+        
     }
-    
+
